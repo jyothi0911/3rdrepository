8407c2e0a6187cbdc7c3e33bbe73bbacc020c233
jyothi0911 <sivanagajyothi522@gmail.com>
2020-07-17 11:36:45 +0530

    Task no is 3

---
+++ src/test/java/org/git3/ProjectGit3.java
@@ -0,0 +1,13 @@
+package org.git3;
+
+public class ProjectGit3 {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("a is" +a);
+		System.out.println("b is" + b);
+		System.out.println("adding number is" + c);
+	}
+
+}
